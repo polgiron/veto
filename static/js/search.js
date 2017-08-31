f195d657f8d9514a9102552ff00e66828fd831cd
Paul Giron <pol.giron@gmail.com>
2017-08-31 14:49:52 +0100

    Add the introduction fiche to the search again

--- static/js/search.js
+++ static/js/search.js
@@ -2,7 +2,8 @@ App.Search = function() {
   $('#search-input').on('keyup', function(event) {
     var val = $(this).val().toLowerCase();
 
-    $('.fiche-nav__entry:not(:first-child)').each(function(index, el) {
+    // $('.fiche-nav__entry:not(:first-child)').each(function(index, el) {
+    $('.fiche-nav__entry').each(function(index, el) {
       if ($(el).text().toLowerCase().indexOf(val) == -1) {
         $(el).hide();
       } else {
