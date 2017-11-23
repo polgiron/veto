86f9db26814f4c35301375b61c7912926aa0af89
MalguyMQ <malguy.quentin@gmail.com>
2017-11-23 19:48:05 +0100

    add log test

--- models/Fiche.js
+++ models/Fiche.js
@@ -7,18 +7,19 @@ var Types = keystone.Field.Types;
  */
 
 var Fiche = new keystone.List('Fiche', {
-  autokey: { from: 'name', path: 'slug', unique: true },
-  sortable: true
+	autokey: { from: 'name', path: 'slug', unique: true },
+	sortable: true,
 });
 
 Fiche.add({
-  name: { type: String, required: true },
-  state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-  publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-  brief: { type: Types.Textarea, height: 200 },
-  content: { type: Types.Html, wysiwyg: true, height: 400 },
-  category: { type: Types.Relationship, ref: 'FicheCategory' },
-  isHighlight: { type: Boolean, label: 'Sur la page d\'accueil?', index: true },
+	name: { type: String, required: true },
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+	brief: { type: Types.Textarea, height: 200 },
+	content: { type: Types.Html, wysiwyg: true, height: 400 },
+	category: { type: Types.Relationship, ref: 'FicheCategory' },
+	isHighlight: { type: Boolean, label: 'Sur la page d\'accueil?', index: true },
+	view: { type: Number, default: 0 },
 });
 
 Fiche.register();
--- routes/views/fiche.js
+++ routes/views/fiche.js
@@ -1,102 +1,103 @@
 var keystone = require('keystone');
 var async = require('async');
 
-exports = module.exports = function(req, res) {
-  var view = new keystone.View(req, res);
-  var FicheCategory = keystone.list('FicheCategory');
-  var Fiche = keystone.list('Fiche');
-  var locals = res.locals;
+exports = module.exports = function (req, res) {
+	var view = new keystone.View(req, res);
+	var FicheCategory = keystone.list('FicheCategory');
+	var Fiche = keystone.list('Fiche');
+	var locals = res.locals;
 
-  locals.section = 'fiches';
+	locals.section = 'fiches';
 
-  var Page = keystone.list('Page');
+	var Page = keystone.list('Page');
 
-  view.on('init', function(next) {
-    var q = Page.model.findOne({
-      type: 'home'
-    });
+	view.on('init', function (next) {
+		var q = Page.model.findOne({
+			type: 'home',
+		});
 
-    q.exec(function(err, results) {
-      if (results) {
-        locals.phone = results.phone;
-        locals.email = results.email;
-      }
-      next(err);
-    });
-  });
+		q.exec(function (err, results) {
+			if (results) {
+				locals.phone = results.phone;
+				locals.email = results.email;
+			}
+			next(err);
+		});
+	});
 
   // Page intro
 
-  view.on('init', function(next) {
-    var q = Page.model.findOne({
-      type: 'fiche'
-    });
+	view.on('init', function (next) {
+		var q = Page.model.findOne({
+			type: 'fiche',
+		});
 
-    q.exec(function(err, results) {
-      if (results) {
-        locals.data = results;
-      }
-      next(err);
-    });
-  });
+		q.exec(function (err, results) {
+			if (results) {
+				locals.data = results;
+			}
+			next(err);
+		});
+	});
 
   // Fiches for the menu
 
-  view.on('init', function(next) {
-    var q = FicheCategory.model.find().sort('sortOrder');
+	view.on('init', function (next) {
+		var q = FicheCategory.model.find().sort('sortOrder');
 
-    q.exec(function(err, results) {
-      if (err || !results.length) {
-        return next(err);
-      }
+		q.exec(function (err, results) {
+			if (err || !results.length) {
+				return next(err);
+			}
 
-      locals.data.categories = results;
+			locals.data.categories = results;
 
-      next(err);
-    });
-  });
+			next(err);
+		});
+	});
 
-  view.on('init', function(next) {
-    var q = Fiche.model.find({
-      state: 'published'
-    }).populate('category').sort('sortOrder');
+	view.on('init', function (next) {
+		var q = Fiche.model.find({
+			state: 'published',
+		}).populate('category').sort('sortOrder');
 
-    q.exec(function(err, fiches) {
-      if (fiches) {
-        locals.data.fiches = fiches;
-      }
+		q.exec(function (err, fiches) {
+			if (fiches) {
+				locals.data.fiches = fiches;
+			}
 
-      locals.activeFiche = 'introduction';
+			locals.activeFiche = 'introduction';
 
-      if (req.params.fiche) {
-        locals.activeFiche = req.params.fiche;
-      } else {
-        return res.redirect('/fiches/' + locals.data.fiches[0].slug);
-      }
+			if (req.params.fiche) {
+				locals.activeFiche = req.params.fiche;
+			} else {
+				return res.redirect('/fiches/' + locals.data.fiches[0].slug);
+			}
 
-      next(err);
-    });
-  });
+			next(err);
+		});
+	});
 
   // Fiche
 
-  if (req.params.fiche) {
-    view.on('init', function(next) {
-      var q = Fiche.model.findOne({
-        slug: req.params.fiche
-      }).populate('category');
+	if (req.params.fiche) {
+		view.on('init', function (next) {
+			var q = Fiche.model.findOne({
+				slug: req.params.fiche,
+			}).populate('category');
 
-      q.exec(function(err, fiche) {
-        if (fiche) {
-          locals.fiche = fiche;
-        } else {
-          req.flash('error', 'Cette fiche n\'existe pas.');
-          return res.redirect('/fiches');
-        }
-        next(err);
-      });
-    });
-  }
+			q.exec(function (err, fiche) {
+				if (fiche) {
+					console.log('fiche', fiche);
+					locals.fiche = fiche;
+				} else {
+					req.flash('error', 'Cette fiche n\'existe pas.');
+					return res.redirect('/fiches');
+				}
+				next(err);
+			});
+		});
+	}
 
-  view.render('fiche');
+	view.render('fiche');
 };
