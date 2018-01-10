c8ff017d655876f9e62b6de2a20e7f1f9159a28f
MalguyMQ <malguy.quentin@gmail.com>
2018-01-10 11:58:16 +0100

    fix url

--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,23 @@ keystone.pre('render', middleware.flashMessages);
 
 // Import Route Controllers
 var routes = {
-  views: importRoutes('./views'),
+	views: importRoutes('./views'),
 };
 
 // Setup Route Bindings
 exports = module.exports = function (app) {
   // Views
-  app.get('/', routes.views.index);
-  app.get('/fiches', routes.views.fiche);
-  app.get('/fiches/:fiche', routes.views.fiche);
-  app.get('/team', routes.views.team);
-  app.get('/clinique', routes.views.clinique);
-  app.get('/service', routes.views.service);
-  app.all('/contact', routes.views.contact);
-  app.get('/david.znaty', routes.views.perso);
+	app.get('/veterinaires-paris', function (req, res) {
+		res.redirect('/');
+	});
+	app.get('/', routes.views.index);
+	app.get('/fiches', routes.views.fiche);
+	app.get('/fiches/:fiche', routes.views.fiche);
+	app.get('/team', routes.views.team);
+	app.get('/clinique', routes.views.clinique);
+	app.get('/service', routes.views.service);
+	app.all('/contact', routes.views.contact);
+	app.get('/david.znaty', routes.views.perso);
 
   // app.get('/admin', routes.views.index);
 };
